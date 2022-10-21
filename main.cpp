e9bb20a7f0fa82b7be6f601cc7de7edfa075d2b1
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-10-20 22:38:31 -0400

    added testing to main file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <cctype>
+#include <string>
 #include "unindent.h"
 
 int main()
 {
-	std::cout << "testing" << std::endl;
+	std::string str = "";
+	std::string correctStr;
+	std::ifstream in_file("bad-code.cpp");
+	while (std::getline(in_file, str))
+	{
+		correctStr += removeLeadingSpaces(str);
+		correctStr += "\n";
+	}
+	std::cout << correctStr << std::endl;
+	return 0;
 }
--- unindent.cpp
+++ unindent.cpp
@@ -7,31 +7,15 @@ std::string removeLeadingSpaces(std::string line)
 {
 	std::string str = "";
 	int count = 0;
-	while (isspace(std::stoi(line.substr(count))))
+	while (isspace(line[count]))
 	{
 		count++;
-
 	}
 	for (int i=count; i<line.length(); i++)
 	{
-		str += line.substr(i);
+		str += line[i];
 	}
 	return str;
 }
 
-int main()
-{
-	std::string str = "";
-	std::string file = "";
-	std::string correctStr;
-	std::cin >> file;
-	std::ifstream in_file(file);
-	while (getline(in_file, str))
-	{
-		correctStr += removeLeadingSpaces(str);
-		correctStr += "\n";
-	}
-	std::cout << correctStr << std::endl;
-	return 0;
-}
 
