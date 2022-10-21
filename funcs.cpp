040528c88eba0c868f2e4261fb09d4494a137333
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-10-21 06:09:02 -0400

    cleaned files and updated info

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <cctype>
 #include <string>
 #include "funcs.h"
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <cctype>
 #include <string>
 #include "funcs.h"
 
